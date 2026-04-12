215b8fa2b0e14b10b1b4ad68f68e14944cd2536b
unknown <kpeminibeatrice18@gmail.com>
2026-04-12 01:51:14 -1200

    Finished cards and checkout

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,7 +1,13 @@
 import {Link} from "react-router-dom"
+import { useCart } from "../context/CartContext";
 
 function ProductCard ({product}) {
+    const {addToCart, cartItems} = useCart();
+    const productInCart = cartItems.find((item) => item.id === product.id);
 
+    const productQuantityLabel = productInCart 
+        ? `(${productInCart.quantity})` 
+        : "";
     return (
         <div>
             <div className="product-card">
@@ -17,7 +23,12 @@ function ProductCard ({product}) {
                             <Link className="btn btn-secondary" to={`/products/${product.id}`}>
                                 View Details
                             </Link>
-                            <button className="btn btn-primary">Add to Cart</button>
+                            <button 
+                                className="btn btn-primary" 
+                                onClick={() => addToCart(product.id)}
+                            >
+                                Add to Cart {productQuantityLabel}
+                            </button>
                         </div>
                     </div>
             </div>
